381a8f5bac06a7d8748c0f8fb95020a16f3417c4
Danh1304 <thaiannhien@gmail.com>
2019-06-11 20:23:49 +0700

    create user model

--- models/users.js
+++ models/users.js
@@ -1,15 +1,22 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
 const UserSchema = new mongoose.Schema({
-    email: {type: String, required: true},
-    password: {type: String, required: true},
-    fullName: {type: String, required: true},
-    userType: {type: String, required: true},
-    phone: {type: String, required: true},
-    dateOfBirth: {type: String, required: true},
-    registerDate: {type: String, required: true},
-    numberOfTrips: {type: String, required: true},
-    numberOfKms: {type: String, required: true},
-    avatar: {type: String, required: true},
-    isActive: {type: Boolean, required: true},
-})
+  email: { type: String, required: true },
+  password: { type: String, required: true },
+  fullName: { type: String, required: true },
+  userType: { type: String, required: true },
+  phone: { type: String, required: true },
+  dateOfBirth: { type: String, required: true },
+  registerDate: { type: Date, default: new Date().getTime() },
+  numberOfTrips: { type: Number },
+  numberOfKms: { type: Number },
+  avatar: { type: String },
+  isActive: { type: Boolean, default: true }
+});
+
+const User = mongoose.model("User", UserSchema);
+
+module.exports = {
+  UserSchema,
+  User
+};
